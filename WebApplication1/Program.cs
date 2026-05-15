269bf435c80b4704eaea2f309b8c9f06ad0f4b0b
usmanghouri <165643098+usmanghouri@users.noreply.github.com>
2026-05-15 20:21:18 +0500

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,43 +1,23 @@
-using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// Register the DbContext for PostgreSQL
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-});
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseCors("AllowAll");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// ... existing code ...
 
 app.MapControllers();
+
+// --- Add this block to auto-migrate on startup ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        // This applies any pending migrations and creates the DB if it doesn't exist
+        context.Database.Migrate(); 
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+// --------------------------------------------------
 
 app.Run();
